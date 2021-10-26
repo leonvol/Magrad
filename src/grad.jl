47ada90ffa8d3d01ab716a76fa2de00e717163d9
Leon Voland <31935319+leonvol@users.noreply.github.com>
2021-10-26 13:13:58 +0200

    Generate grad calculation methods

--- src/grad.jl
+++ src/grad.jl
@@ -14,23 +14,30 @@ end
 # Dispatch on op type
 import Base.Broadcast: BroadcastFunction
 
-function calculategrad!(::Type{BroadcastFunction{typeof(+)}}, 
-                        t::Tensor, u::Union{Tensor,AbstractArray,Number}, res::Tensor)
-    t.grad .+= res.grad
-    addgrad!(u, res.grad)
-    return nothing
-end
+# Generate methods to calculate gradients for every order of arguments 
+for (T, S) in ((Tensor, Union{AbstractArray,Number}),
+                (Union{AbstractArray,Number}, Tensor),
+                (Tensor, Tensor)) # Needed to fix ambiguity
+    @eval begin
+        function calculategrad!(::Type{BroadcastFunction{typeof(+)}}, 
+                                t::$T, u::$S, res::Tensor)
+            addgrad!(t, res.grad)
+            addgrad!(u, res.grad)
+            return nothing
+        end
 
-function calculategrad!(::Type{BroadcastFunction{typeof(-)}},
-                        t::Tensor, u::Union{Tensor,AbstractArray,Number}, res::Tensor)
-    t.grad .+= res.grad
-    addgrad!(u, -res.grad)
-    return nothing
-end
+        function calculategrad!(::Type{BroadcastFunction{typeof(-)}},
+                                t::$T, u::$S, res::Tensor)
+            addgrad!(t, res.grad)
+            addgrad!(u, -res.grad)
+            return nothing
+        end
 
-function calculategrad!(::Type{BroadcastFunction{typeof(*)}}, 
-                                t::Tensor, u::Union{Tensor,AbstractArray}, res::Tensor)   
-    t.grad .+= res.grad .* data(u)
-    addgrad!(u, res.grad .* t.data)
-    return nothing
+        function calculategrad!(::Type{BroadcastFunction{typeof(*)}}, 
+                                t::$T, u::$S, res::Tensor)   
+            addgrad!(t, res.grad .* data(u))
+            addgrad!(u, res.grad .* data(t))
+            return nothing
+        end
+    end
 end
